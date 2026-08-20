1610de59dc4a889275ff23e2f1b12c4287de69ac
Souvik Ghosh <souvikg158@gmail.com>
2026-08-20 22:31:10 +0530

    FIX API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,27 @@
 import axios from "axios";
 
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://papertrade-stock-trading-platform-1.onrender.com/api";
+
 export const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-API.interceptors.request.use(config => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
