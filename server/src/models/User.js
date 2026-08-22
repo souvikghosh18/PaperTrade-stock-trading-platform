21f7d57a9a04970d0107b55472f51c4270ed5c5d
Souvik Ghosh <souvikg158@gmail.com>
2026-08-22 18:30:22 +0530

    update user model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
+  username: { type: String, required: true, unique: true, lowercase: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
   password: { type: String, required: true },
   cash: { type: Number, default: 100000 },
